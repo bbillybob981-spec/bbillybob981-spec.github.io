13d3b95506c8b1519b6fcfa83656df630cb84138
Full Stack Demon <bbillybob981@gmail.com>
2026-02-25 20:08:11 -0500

    Add color styling to paragraph class list elements

---
+++ script.js
@@ -0,0 +1,6 @@
+let p1=document.querySelector('p');
+let array=p1.classList;
+let array1=array.value;
+let array2=array1.split(seperator=" "); 
+for(x=0; x<100; x=x+1) {
+  array[x].style.color=array[x];}
