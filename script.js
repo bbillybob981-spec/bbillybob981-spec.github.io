ce726fb2a6b0260ba950c5ba44153b2bfc4fcdfd
Full Stack Demon <bbillybob981@gmail.com>
2026-02-25 20:35:55 -0500

    Correctly set paragraph color from class names
    Fix color assignment to paragraphs based on class names.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let paragraphs = document.querySelectorAll('p');
 for (const paragraph of paragraphs) {
   for (const className of paragraph.classList) {
-    paragraph.style.color = className;
+    if (className.includes('color-'){
+    let set=className.replace('color-','');
+    paragraph.style.color = className;}
   }
 }
