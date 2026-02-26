b96cd9038af03e8b4c140d693496849ef29b3421
Full Stack Demon <bbillybob981@gmail.com>
2026-02-25 20:21:58 -0500

    Refactor paragraph color assignment logic

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let p1=document.querySelector('p');
-let array=[];
-for (const paragraph of p1) {
-    const classList = paragraph.classList;
-    array.push(classList);}
-let array1=array.value;
-let array2=array1.split(seperator=" "); 
-for(x=0; x<100; x=x+1) {
-  array[x].style.color=array[x];}
+let paragraphs = document.querySelectorAll('p');
+for (const paragraph of paragraphs) {
+  for (const className of paragraph.classList) {
+    paragraph.style.color = className;
+  }
+}
