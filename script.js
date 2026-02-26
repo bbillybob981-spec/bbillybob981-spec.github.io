79c87af9a9ec025265a6e340786fc852a4b10cf1
Full Stack Demon <bbillybob981@gmail.com>
2026-02-25 20:15:38 -0500

    Refactor script.js to handle class lists
    Initialize array and push class lists from paragraphs.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let p1=document.querySelector('p');
-let array=p1.classList;
+let array=[];
+for (const paragraph of p1) {
+    const classList = paragraph.classList;
+    array.push(classList);}
 let array1=array.value;
 let array2=array1.split(seperator=" "); 
 for(x=0; x<100; x=x+1) {
