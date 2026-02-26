508a274e7a3dd91b2f275b65b2090d37d501308b
Full Stack Demon <bbillybob981@gmail.com>
2026-02-25 20:39:50 -0500

    Fix syntax error in color class check

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let paragraphs = document.querySelectorAll('p');
 for (const paragraph of paragraphs) {
   for (const className of paragraph.classList) {
-    if (className.includes('color-'){
+    if (className.includes('color-')) {
     let set=className.replace('color-','');
     paragraph.style.color = className;}
   } 
