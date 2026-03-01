596ef940b882b62a6a181ceb5c2cf1316d4ce1c0
Full Stack Demon <bbillybob981@gmail.com>
2026-03-01 16:00:08 -0500

    Fix className replacements for CSS styles

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ let paragraphs = document.querySelectorAll('*');
 for (const paragraph of paragraphs) {
   for (const className of paragraph.classList) {
     if (className.includes('fontsize-')){
-      let set=className.replace('fontsize-','');
+      let set=className.replace('fontSize-','');
       paragraph.style.fontSize=set+"px";}
     if (className.includes('padding-')){
       let set=className.replace('padding-','');
-      paragraph.style.padding=set;}
+      paragraph.style.padding=set+"px";}
     if (className.includes('margin-')){
       let set=className.replace('margin-','');
-      paragraph.style.margin=set;}
+      paragraph.style.margin=set+"px";}
+    if (className.includes('backgroundColor-')){
+      let set=className.replace('backgroundColor-','');
+      paragraph.style.backgroundColor=set+;}
     if (className.includes('color-')) {
     let set=className.replace('color-','');
     paragraph.style.color = set;}
